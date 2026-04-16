ffdf2b5721b941edab5d8eef854a12d6cb7a1ea9
jab-wtamu <jabarrios2@buffs.wtamu.edu>
2026-04-15 19:06:19 -0500

    Add customPDE class for matrix-free PDE implementation
    This file defines a custom PDE class inheriting from MatrixFreePDE, including methods for initial conditions, boundary conditions, and governing equations.

---
+++ customPDE.h
@@ -0,0 +1,109 @@
+#include <core/matrixFreePDE.h>
+
+using namespace dealii;
+
+template <int dim, int degree>
+class customPDE : public MatrixFreePDE<dim, degree>
+{
+public:
+  // Constructor
+  customPDE(userInputParameters<dim> _userInputs)
+    : MatrixFreePDE<dim, degree>(_userInputs)
+    , userInputs(_userInputs)
+  {};
+
+  // Function to set the initial conditions (in ICs_and_BCs.h)
+  void
+  setInitialCondition([[maybe_unused]] const Point<dim>  &p,
+                      [[maybe_unused]] const unsigned int index,
+                      [[maybe_unused]] double            &scalar_IC,
+                      [[maybe_unused]] Vector<double>    &vector_IC) override;
+
+  // Function to set the non-uniform Dirichlet boundary conditions (in
+  // ICs_and_BCs.h)
+  void
+  setNonUniformDirichletBCs([[maybe_unused]] const Point<dim>  &p,
+                            [[maybe_unused]] const unsigned int index,
+                            [[maybe_unused]] const unsigned int direction,
+                            [[maybe_unused]] const double       time,
+                            [[maybe_unused]] double            &scalar_BC,
+                            [[maybe_unused]] Vector<double>    &vector_BC) override;
+
+private:
+#include <core/typeDefs.h>
+
+  const userInputParameters<dim> userInputs;
+
+  // Function to set the RHS of the governing equations for explicit time
+  // dependent equations (in equations.h)
+  void
+  explicitEquationRHS(
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+      &variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double>             element_volume) const override;
+
+  // Function to set the RHS of the governing equations for all other equations
+  // (in equations.h)
+  void
+  nonExplicitEquationRHS(
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+      &variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double>             element_volume) const override;
+
+  // Function to set the LHS of the governing equations (in equations.h)
+  void
+  equationLHS(
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+      &variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double>             element_volume) const override;
+
+  // Function to set postprocessing expressions (in postprocess.h)
+#ifdef POSTPROCESS_FILE_EXISTS
+  void
+  postProcessedFields(
+    [[maybe_unused]] const variableContainer<dim, degree, VectorizedArray<double>>
+      &variable_list,
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+      &pp_variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double>             element_volume) const override;
+#endif
+
+  // Function to set the nucleation probability (in nucleation.h)
+#ifdef NUCLEATION_FILE_EXISTS
+  double
+  getNucleationProbability([[maybe_unused]] variableValueContainer variable_value,
+                           [[maybe_unused]] double                 dV) const override;
+#endif
+
+  // ================================================================
+  // Model constants specific to this subclass
+  // ================================================================
+
+  // Initial reduced supersaturation
+  double u0 = userInputs.get_model_constant_double("u0");
+
+  // Surface-tension anisotropy constants
+  double eps_xy = userInputs.get_model_constant_double("eps_xy");
+  double eps_z  = userInputs.get_model_constant_double("eps_z");
+
+  // Vertical / horizontal anisotropy parameter
+  double Gamma = userInputs.get_model_constant_double("Gamma");
+
+  // Coupling constant
+  double lambda = userInputs.get_model_constant_double("lambda");
+
+  // Reduced diffusion coefficient
+  double D_tilde = userInputs.get_model_constant_double("D_tilde");
+
+  // Vapour depletion / attachment parameter
+  double Lsat = userInputs.get_model_constant_double("Lsat");
+
+  // Small regularization parameter
+  double regval = userInputs.get_model_constant_double("regval");
+
+  // ================================================================
+};
